2ace4c6c25e1c8873cc46689c008901ef8d74016
jeremy <jeremy@debian-BULLSEYE-live-builder-AMD64>
2022-05-03 09:11:05 +0200

    not work

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 
 
@@ -6,28 +6,29 @@ import { View, StyleSheet } from "react-native";
 
 const CreateEvent = () => {
 
-    try {
-        fetch('https://jeremy-dejoux.students-laplateforme.io/api/events', {
-            method: 'post',
-            mode: 'no-cors',
-            headers:{
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "name": "philou",
-                "content": "string",
-                "user": "/api/users/1",
-                "capacity": 0,
-                "dateStart": "2022-01-25T16:20:34.712Z",
-                "dateEnd": "2022-01-25T16:22:34.712Z",
-                "city": "string",
-                "price": 0
-            })
-        })
-    } catch(e){
-        console.log(e)
-    }
+
+    // try {
+    //     fetch('https://jeremy-dejoux.students-laplateforme.io/api/events', {
+    //         method: 'post',
+    //         mode: 'no-cors',
+    //         headers:{
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             "name": "2 Test",
+    //             "content": "string",
+    //             "user": "/api/users/1",
+    //             "capacity": 0,
+    //             "dateStart": "2022-01-25T16:20:34.712Z",
+    //             "dateEnd": "2022-01-25T16:22:34.712Z",
+    //             "city": "string",
+    //             "price": 0
+    //         })
+    //     })
+    // } catch(e){
+    //     console.log(e)
+    // }
 
     return (
         <View>
---
+++ src/pages/ImageUpload.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ImageUpload = () => {
+  return (
+    <View></View>
+  )
+}
+
+export default ImageUpload
