9f49bee8e028f8aa6022a2b74b84d9f8a26a9778
jeremy <jeremy@debian-BULLSEYE-live-builder-AMD64>
2022-05-09 16:31:09 +0200

    App good

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -7,31 +7,9 @@ import { View, StyleSheet } from "react-native";
 const CreateEvent = () => {
 
 
-    // try {
-    //     fetch('https://jeremy-dejoux.students-laplateforme.io/api/events', {
-    //         method: 'post',
-    //         mode: 'no-cors',
-    //         headers:{
-    //             'Accept': 'application/json',
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({
-    //             "name": "2 Test",
-    //             "content": "string",
-    //             "user": "/api/users/1",
-    //             "capacity": 0,
-    //             "dateStart": "2022-01-25T16:20:34.712Z",
-    //             "dateEnd": "2022-01-25T16:22:34.712Z",
-    //             "city": "string",
-    //             "price": 0
-    //         })
-    //     })
-    // } catch(e){
-    //     console.log(e)
-    // }
 
     return (
-        <View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'green' }}>
 
         </View>
     )
