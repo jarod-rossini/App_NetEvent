676af683212c57287f20f1ad2c1ac8c82d547fac
emmanuel cabassot <emmanuel.cabassot@laplateforme.io>
2022-06-20 16:29:23 +0200

    design screen Connection

--- src/screens/ConnectionScreen.js
+++ src/screens/ConnectionScreen.js
@@ -1,9 +1,17 @@
 import * as React from "react";
-import { View, Text, Button, TextInput, SafeAreaView, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { AuthContext } from "../context/context.js";
 import { useContext, useState } from "react";
-
+import { Foundation } from "@expo/vector-icons";
 
 export default ConnectionScreen = () => {
   const navigation = useNavigation();
@@ -12,55 +20,89 @@ export default ConnectionScreen = () => {
 
   const { signIn } = useContext(AuthContext);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white",
-      }}
-    >
-      <SafeAreaView>
+    <View style={styles.view}>
+      <View style={styles.viewKey}>
+        <Foundation name="key" size={50} color="white" />
+      </View>
+      <View style={styles.viewForm}>
         <TextInput
-        style={styles.input}
+          style={styles.input}
           placeholder="Username"
           value={username}
           onChangeText={setUsername}
         />
+
         <TextInput
-        style={styles.input}
+          style={styles.input}
           placeholder="Password"
           value={password}
           onChangeText={setPassword}
           secureTextEntry
         />
-        <Button
-          style={styles.submit}
-          title="Sign in"
+        <Pressable
+          style={styles.buttonConnexion}
           onPress={() => signIn({ username, password })}
-        />
-        <Button
-          style={styles.submit}
-          title="Inscription"
-          onPress={() =>
-            navigation.navigate("Inscription")
-          }
-        />
-      </SafeAreaView>
+        >
+          <Text style={styles.textConnexion}>CONNEXION</Text>
+        </Pressable>
+      </View>
+      <View styles={styles.viewInscription}>
+        <Pressable
+          style={styles.buttonInscription}
+          onPress={() => navigation.navigate("Inscription")}
+        >
+          <Text style={styles.textInscription}>INSCRIPTION</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  viewKey: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "30%",
+  },
+  viewForm: {
+    alignItems: "center",
+    justifyContent: "flex-start",
+    height: "55%",
+  },
   input: {
     height: 40,
     width: 200,
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    backgroundColor: "white",
   },
-  submit: {
-    width: 150,
-    marginBottom: 100,
-    backgroundColor: 'red',
+  textInscription: {
+    color: "white",
+  },
+  textConnexion: {
+    color: "grey",
+  },
+  buttonInscription: {
+    width: "40%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 15,
+    elevation: 3,
+    backgroundColor: "red",
+  },
+  buttonConnexion: {
+    width: "40%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 15,
+    elevation: 3,
+    backgroundColor: "white",
   },
 });
